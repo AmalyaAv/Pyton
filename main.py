605a8771ec403c010e12c1496249da0037488ec9
AmalyaAv <amalyaav09@gmail.com>
2023-06-24 17:15:16 +0500

    h

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+access = False
+while not access:
+    username = input("Enter your username")
+    password = input("Enter your password")
+    if username == "Amaliya" and password == "12345":
+        print("Access granted")
+        access = True
+    elif username == "Amaliya":
+        print("Access denied, Wrong password")
+    else:
+        print("Access denied, Wrong username")
+print("End of program")
